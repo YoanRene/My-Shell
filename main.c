db69220824b436de2b9b18271d17129c64f6a95e
Cyoan <106040694+YoanRene@users.noreply.github.com>
2023-04-25 02:21:10 -0400

    cambiando mi structure por fork

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 #include <ctype.h>
 
 /*
-Integrantes: 
+Integrantes:
 Yoan Rene Ramos Corrales - C312
 Kevin Majim Ortega Alvarez - C312
 
@@ -22,7 +22,7 @@ cd: cambia de directorios
 exit: termina el shell
 help: muestra la ayuda del shell
 ls: muesta la lista de archivos y carpetas de un directorio
-echo: muestra el texto entrado despues del comando 
+echo: muestra el texto entrado despues del comando
 
 Total: 4 puntos*/
 
@@ -31,7 +31,10 @@ Total: 4 puntos*/
 
 char pegado[BUFSIZ];
 char auxiliar1[BUFSIZ];
+char out_file[] = "out7.txt";
 
+char out_final[BUFSIZ];
+int opens[20];
 char pwd[BUFSIZ];             //
 char cadena[BUFSIZ];          //
 char args[BUFSIZ];            // Importantes
@@ -39,9 +42,14 @@ char splited[TAMANO][BUFSIZ]; //
 
 char splited_aux[TAMANO][BUFSIZ];
 int command_2nd_index;
-int count_tuberias;
 int index_command;
 char cadena_aux[BUFSIZ];
+
+int fd_r,fd_w;
+int a, b, c, d, e, f, x;
+int k, h;
+int hay_in_out;
+
 
 void reset_splited()
 {
@@ -109,6 +117,9 @@ void recuperador_de_path_function_with_split()
 
 void echo_function()
 {
+    printf("%s", "kaka\n ");
+    // strcat(args, "/My-Shell");
+    // sleep(5);
     printf("%s", args);
 }
 
@@ -120,6 +131,17 @@ void pwd_fuction()
 void exit_function()
 {
     exit(EXIT_SUCCESS);
+}
+
+void cerrador()
+{
+    int k = 0;
+    while (k < 20 && opens[k] != 0)
+    {
+        close(opens[k]);
+        opens[k] = 0;
+        k++;
+    }
 }
 
 void ls_function()
@@ -139,16 +161,14 @@ void ls_function()
 
 void help_function()
 {
-    //int fd = open("help.txt", O_RDONLY);
+    // int fd = open("help.txt", O_RDONLY);
 
-    //read(fd, out, BUFSIZ);
-    //close(fd);
+    // read(fd, out, BUFSIZ);
+    // close(fd);
 }
 
 void cd_function()
 {
-
-    strcpy(args, splited[index_command + 1]);
 
     if (SON_IGUALES(args, ""))
     {
@@ -177,87 +197,145 @@ void limpieza_de_signo_de_numero()
     }
 }
 
+void tareas_de_entradas_salidas(){
+    while (SON_IGUALES(splited[k], ">") || SON_IGUALES(splited[k], ">>") || SON_IGUALES(splited[k], "<"))
+        {
+            if (SON_IGUALES(splited[k], ">"))
+            {
+                fd_w = open(splited[k + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                opens[h++] = fd_w;
+                if (fd_w != -1)
+                    a = dup2(fd_w, STDOUT_FILENO);
+                else
+                    perror("error");
+            }
+            else if (SON_IGUALES(splited[k], ">>"))
+            {
+                fd_w = open(splited[k + 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
+                opens[h++] = fd_w;
+                if (fd_w != -1)
+                    b = dup2(fd_w, STDOUT_FILENO);
+                else
+                    perror("error");
+            }
+            else
+            {
+                fd_r = open(splited[k + 1], O_RDONLY);
+                if (fd_r != -1)
+                    read(fd_r, args, BUFSIZ);
+                close(fd_r);
+            }
+            k += 2;
+        }
+}
+
+void selector(){
+    if (SON_IGUALES(splited[index_command], "ls"))
+            ls_function();
+        else if (SON_IGUALES(splited[index_command], "exit"))
+            exit_function();
+        else if (SON_IGUALES(splited[index_command], "pwd"))
+            pwd_fuction();
+        else if (SON_IGUALES(splited[index_command], "cd"))
+            cd_function();
+        else if (SON_IGUALES(splited[index_command], "help"))
+            help_function();
+        else if (SON_IGUALES(splited[index_command], "echo"))
+            echo_function();
+        else
+        {
+            if (isalnum(splited[index_command][0]) == 0)
+                printf("%s", "Error de sintaxis");
+            else
+                printf("%s", "Comando desconocido"); // fprintf(stderr,"ERROR");exit(1);
+        }
+
+}
+
+void comprobador_de_in_out(){
+    hay_in_out = (k != index_command + 1);
+        if (!hay_in_out)
+        {
+            if (!SON_IGUALES(splited[k], "|") && !SON_IGUALES(splited[k], ""))
+            {
+                strcpy(args, splited[k]);
+            }
+            k++;
+        }
+}
+
+void detector_de_2nd_command(){
+    for (int i = 0; i < TAMANO; i++)
+    {
+        if(SON_IGUALES(splited[i],"|")){
+            command_2nd_index=i+1;
+            return;
+        }
+    }
+    
+}
+
 int main()
 {
     getcwd(pwd, BUFSIZ);
+    int sigue_andando=1;
 
-    while (1)
+    while (sigue_andando)
     {
         index_command = 0;
-        count_tuberias = 0;
-        int fd_r;
-        int fd_w;
+        command_2nd_index=0;
+
+        int tuberia[2];
+        pipe(tuberia);
 
         printf("my-prompt $ ");
         fgets(cadena, BUFSIZ, stdin);
 
         limpieza_de_signo_de_numero();
         recuperador_de_path_function_with_split();
+        detector_de_2nd_command();
         args[0] = '\0';
 
-        //OK
-        //int std_ini = dup(STDIN_FILENO);
+        // OK
+        // int std_ini = dup(STDIN_FILENO);
         int std_out = dup(STDOUT_FILENO);
-        //OK
-        while (count_tuberias >= 0)
-        {
-            int k = index_command+1;
-            while (SON_IGUALES(splited[k], ">")||SON_IGUALES(splited[k], ">>")||SON_IGUALES(splited[k], "<"))
-            {
-                if (SON_IGUALES(splited[k], ">"))
-                {
-                    fd_w = open(splited[k + 1], O_WRONLY | O_CREAT | O_TRUNC);
-                    if (fd_w != -1)
-                        dup2(fd_w, STDOUT_FILENO);
-                }
-                else if (SON_IGUALES(splited[k], ">>"))
-                {
-                    fd_w = open(splited[k + 1], O_WRONLY | O_CREAT | O_APPEND);
-                    if (fd_w != -1)
-                        dup2(fd_w, STDOUT_FILENO);
-                }
-                else
-                {
-                    fd_r = open(splited[k + 1], O_RDONLY);
-                    if (fd_r != -1)
-                        read(fd_r,args,BUFSIZ);
-                    close(fd_r);
-                }
-                k+=2;
-            }
-            //OK
 
-            int hay_in_out=(k!=1);
-            if(!hay_in_out){
-                if(!SON_IGUALES(splited[k],"|"))
-                    strcpy(args,splited[k]);
-            }
+        pid_t pid = fork();
+        if(pid==0){
+            if(command_2nd_index==0)
+                return 0;
+            close(tuberia[0]);
+            dup2(tuberia[1],STDOUT_FILENO);
 
-            if (SON_IGUALES(splited[index_command], "ls"))
-                ls_function();
-            else if (SON_IGUALES(splited[index_command], "exit"))
-                exit_function();
-            else if (SON_IGUALES(splited[index_command], "pwd"))
-                pwd_fuction();
-            else if (SON_IGUALES(splited[index_command], "cd"))
-                cd_function();
-            else if (SON_IGUALES(splited[index_command], "help"))
-                help_function();
-            else if (SON_IGUALES(splited[index_command], "echo"))
-                echo_function();
-            else
-            {
-                if (isalnum(splited[index_command][0]) == 0)
-                    printf("%s", "Error de sintaxis");
-                else
-                    printf("%s", "Comando desconocido"); // fprintf(stderr,"ERROR");exit(1);
-            }
-            index_command = command_2nd_index;
-           // dup2(std_ini, STDIN_FILENO);
-            dup2(std_out, STDOUT_FILENO);
-            count_tuberias--;
+            k = index_command + 1;
+            h = 0;
+            sigue_andando=0;
+            
+            tareas_de_entradas_salidas();
+            comprobador_de_in_out();
+            selector();
+
+            e = dup2(std_out, STDOUT_FILENO);
+            cerrador();
+            close(tuberia[1]);
         }
-        printf("\n");
+        else
+        {
+            close(tuberia[1]);
+            wait(NULL);
+
+            index_command=command_2nd_index;
+            h = 0;
+
+            tareas_de_entradas_salidas();
+            comprobador_de_in_out();
+            selector();
+
+            e = dup2(std_out, STDOUT_FILENO);
+            cerrador();
+            close(tuberia[0]);
+        }
+        printf("\n");        
     }
 
     return 0;
